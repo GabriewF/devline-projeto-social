851e5d9f7010f45adfe26a0246388b6da05ef350
GabriewF <85371036+GabriewF@users.noreply.github.com>
2026-08-10 13:49:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './assets/css/fonts.css'
+
+function App() {
+  return (
+    <div className='bg-black text-white font-hero text-[9rem] flex items-center justify-center h-screen'>
+        <h1>Trabalho em Andamento.</h1>
+    </div>
+  )
+}
+
+export default App
