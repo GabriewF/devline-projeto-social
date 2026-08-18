c08434d98a8066a7e2fc411bb4adb6891157a913
Biel Dev <gabrielgamileira512@gmail.com>
2026-08-18 14:08:33 -0300

    Finalization Cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,13 @@ import "./assets/css/fonts.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Index from "./pages";
-import Teste from "./pages/Teste";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Index
   },
-  {
-    path: "/test",
-    Component: Teste
-  }
+
 ])
 
 function App() {
