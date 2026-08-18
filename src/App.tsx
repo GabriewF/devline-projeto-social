162a63583ca89c79912d2a81733f92ad7df22527
levynascimentoDev <ffzikaorg@gmail.com>
2026-08-18 13:19:28 -0300

    fix: route page teste

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,11 @@ import "./assets/css/fonts.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Index from "./pages";
-import Teste from "./pages/Teste";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Index
-  },
-  {
-    path: "/test",
-    Component: Teste
   }
 ])
 
